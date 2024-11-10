6211e9137203829bd0995be9e06460554aa0cf47
Multimaste1 <multimaste1@gmail.com>
2024-11-10 19:09:04 +0000

    Update OrbMovement.cs

--- ProjecT-Kinesis/Assets/Scripts/OrbMovement.cs
+++ ProjecT-Kinesis/Assets/Scripts/OrbMovement.cs
@@ -5,29 +5,26 @@ using UnityEngine;
 public class OrbofProtection : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public float speed;
     float minX;
     float maxX;
     float minY;
     float maxY;
     void Start()
     {
-        //defines screen boundaries
-        minX = 0;
-        maxX = Screen.width;
-        minY = 0;
-        maxY = Screen.height;
+        
+        
     }
 
     public void FollowMouse() //gets current mouse position and transforms orb to it
     {
         //stores mousePosition vector
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition.x = Mathf.Clamp(mousePosition.x, minX, maxX); //restricts the x position of the mouse between the set boundary
-        mousePosition.y = Mathf.Clamp(mousePosition.y, minY, maxY); //restricts the y position of the mouse between the set boundary
+        Vector2 mousePosition = Input.mousePosition;
+        
 
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = new Vector2(mousePosition.x, mousePosition.y); //moves the orb according to mouse position
+        // Smoothly move orb toward the mouse position
+        transform.position = Vector2.Lerp(transform.position, new Vector2(mousePosition.x, mousePosition.y), speed * Time.deltaTime);
     }
 
     // Update is called once per frame
