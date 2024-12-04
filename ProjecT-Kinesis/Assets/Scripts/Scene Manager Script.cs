8d773ff229c963198819e95c861b4fb5da5c97c1
Multimaste1 <multimaste1@gmail.com>
2024-12-04 20:43:44 +0000

    Update Scene Manager Script.cs
    Changed the script by adding a method for map selection so that scenes can be loaded without being explicitly typed out in the inspector.
    Still kept original method for menus.

--- ProjecT-Kinesis/Assets/Scripts/Scene Manager Script.cs
+++ ProjecT-Kinesis/Assets/Scripts/Scene Manager Script.cs
@@ -2,13 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class SceneManagerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public void LoadScene(string sceneName)
+    public string sceneName;
+
+    // Method to set the scene name (for map selection)
+    public void SetSceneName(string name)
     {
-        SceneManager.LoadScene(sceneName);
+        sceneName = name;
     }
 
+    //method for loading maps 
+    public void LoadScene()
+    {
+        
+        SceneManager.LoadScene(sceneName);
+    }
+    //method for loading menus
+    public void LoadScene(string sceneName)
+    {
+
+        SceneManager.LoadScene(sceneName);
+    }
 }
