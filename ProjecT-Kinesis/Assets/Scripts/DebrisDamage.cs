88e2a8a1d7c222f88ed36c1733a81cf529c33126
Multimaste1 <multimaste1@gmail.com>
2025-01-08 17:44:21 +0000

    Update DebrisDamage.cs
    Now destroys enemy projectile when it collides with something

--- ProjecT-Kinesis/Assets/Scripts/DebrisDamage.cs
+++ ProjecT-Kinesis/Assets/Scripts/DebrisDamage.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class DebrisDamage : MonoBehaviour
 {
-    public PlayerHealth playerhealth;
+
     public int damage;
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+
+        if (collision.gameObject.CompareTag("Player")) //checks if tag of object is Player and if it is runs take damage function from PlayerHealth script
         {
-            playerhealth.takedamage(damage);
+            Debug.Log("hit");
+            collision.gameObject.GetComponent<PlayerHealth>().takedamage(damage);
+            Destroy(gameObject);
         }
+
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            Destroy(gameObject);
+        }
+
+        
     }
     
 }
