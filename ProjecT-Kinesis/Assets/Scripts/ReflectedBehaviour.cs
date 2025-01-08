f80d5453b516b7e9a82e5279618dfb4d7c9abaee
Multimaste1 <multimaste1@gmail.com>
2025-01-08 17:43:56 +0000

    Added Meteor Reflection Script and Meteor Reflection Prefab
    Rotation needs to be sorted and needs to deal damage to the enemy.

---
+++ ProjecT-Kinesis/Assets/Scripts/ReflectedBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectedProjectileBehaviour : MonoBehaviour
+{
+    private GameObject orb;
+    private GameObject enemy;
+    private Rigidbody2D reflectedprojectileRigidBody;
+
+    private float reflectedprojectileVelocity = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        reflectedprojectileRigidBody = GetComponent<Rigidbody2D>(); //accesses rigid body component of reflectprojectile
+        enemy = GameObject.FindGameObjectWithTag("Enemy"); //finds enemy game object
+        Vector3 direction = enemy.transform.position - transform.position; //sets direction for projectile to travel towards player
+        reflectedprojectileRigidBody.velocity = new Vector2(direction.x, direction.y).normalized * reflectedprojectileVelocity;
+
+        float reflectedprojectileRotation = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;//angle of rotation that projectile needs to face towards player; converts from radians to degrees 
+        transform.rotation = Quaternion.Euler(0, 0, reflectedprojectileRotation + 45);//adjusts rotation so that graphic of projectile is correct
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
