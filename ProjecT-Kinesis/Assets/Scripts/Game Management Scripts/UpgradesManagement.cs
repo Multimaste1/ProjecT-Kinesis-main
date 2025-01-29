d86d6ae8591ca046a108757677b9c6b732fe2872
Multimaste1 <multimaste1@gmail.com>
2025-01-29 17:34:17 +0000

    Update UpgradesManagement.cs
    Split upgrade function into individual functions for each upgrade

--- ProjecT-Kinesis/Assets/Scripts/Game Management Scripts/UpgradesManagement.cs
+++ ProjecT-Kinesis/Assets/Scripts/Game Management Scripts/UpgradesManagement.cs
@@ -8,43 +8,109 @@ using TMPro;
 public class UpgradesManagement : MonoBehaviour
 {
 
-    public Sprite[] levels; //array to store the different levels of tbe upgrade bar
-    public Image upgradeBar; //image component that stores the upgrade bar
-    public int index = 0; //stores position of level in the array
+   
 
-    public float coins;
-    private float cost =10;
+    
+    private float healthCost =10; //stores baseCosts of upgrades
+    private float flightCost = 10;
+    private float sizeCost = 10;
+    private float velocityCost = 10;
+
+
     public TextMeshProUGUI coinstext;
+
+
+
+    public Sprite[] upgradeBars; //array to store the different levels of the upgrade bar
+    private Image currentupgradeBar; //image component that stores the upgrade bar
+    public int currentLevel = 0; //stores position of level in the array
+
+    public Coins coins; //allows upgrades to access the Coins scriptable object
 
 
     
     // Start is called before the first frame update
     void Start()
     {
-        upgradeBar = GetComponent<Image>(); // stores image component of the upgrade bar
-        coinstext.text = "Coins:" + coins.ToString();
+        currentupgradeBar = GetComponent<Image>(); // stores image component of the upgrade bar
+
+        coinstext.text = "Coins:" + coins.coinbalance.ToString();
     }
 
     // Update is called once per frame
 
-    public void upgrade() //function to update upgrade bar
+    public void upgradeHealth()
     {
-        if (index < levels.Length && coins>=cost) //if max level isn't reached
+        if (currentLevel < upgradeBars.Length && coins.coinbalance >= (healthCost)) //if max level isn't reached and coin balance > cost
         {
-            upgradeBar.sprite = levels[index];
-            index += 1; //increments through upgrade levels
+            currentupgradeBar.sprite = upgradeBars[currentLevel];
+            currentLevel += 1; //increments through upgrade levels
 
-            coins -= cost; //takes away upgrade cost from amount of coins
-            cost = MathF.Floor(cost *1.5f); //increases upgrade cost by 50%
-            coinstext.text = "Coins:"+coins.ToString(); //changes coin text to display coin total after upgrade purchase
+            coins.coinbalance -= Mathf.FloorToInt(healthCost); //takes away upgrade cost from amount of coins
+            healthCost = healthCost * 1.5f;
+            coinstext.text = "Coins:" + coins.coinbalance.ToString(); //changes coin text to display coin total after upgrade purchase
+            Debug.Log(coins.coinbalance);
         }
         else //if max level (5) is reached 
         {
             Debug.Log("max level");
             return; //exits the function
         }
-       
     }
-    
 
+    public void upgradeFlight()
+    {
+        if (currentLevel < upgradeBars.Length && coins.coinbalance >= (flightCost)) 
+        {
+            currentupgradeBar.sprite = upgradeBars[currentLevel];
+            currentLevel += 1; 
+
+            coins.coinbalance -= Mathf.FloorToInt(flightCost); 
+            flightCost = flightCost * 1.5f;
+            coinstext.text = "Coins:" + coins.coinbalance.ToString(); 
+            Debug.Log(coins.coinbalance);
+        }
+        else 
+        {
+            Debug.Log("max level");
+            return; 
+        }
+    }
+
+    public void upgradeSize()
+    {
+        if (currentLevel < upgradeBars.Length && coins.coinbalance >= (sizeCost)) 
+        {
+            currentupgradeBar.sprite = upgradeBars[currentLevel];
+            currentLevel += 1; 
+
+            coins.coinbalance -= Mathf.FloorToInt(sizeCost); 
+            sizeCost = sizeCost * 1.5f;
+            coinstext.text = "Coins:" + coins.coinbalance.ToString();
+            Debug.Log(coins.coinbalance);
+        }
+        else //if max level (5) is reached 
+        {
+            Debug.Log("max level");
+            return; 
+        }
+    }
+
+    public void upgradeVelocity()
+    {
+        if (currentLevel < upgradeBars.Length && coins.coinbalance >= (velocityCost)) 
+        {
+            currentupgradeBar.sprite = upgradeBars[currentLevel];
+            currentLevel += 1; 
+            coins.coinbalance -= Mathf.FloorToInt(velocityCost); 
+            velocityCost = velocityCost * 1.5f;
+            coinstext.text = "Coins:" + coins.coinbalance.ToString(); 
+            Debug.Log(coins.coinbalance);
+        }
+        else 
+        {
+            Debug.Log("max level");
+            return;
+        }
+    }
 }
