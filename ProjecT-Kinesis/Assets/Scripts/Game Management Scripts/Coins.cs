38abb460496807ca5e828c6a014975759b5ba637
Multimaste1 <multimaste1@gmail.com>
2025-01-29 17:35:34 +0000

    Created Coins Scriptable Object
    Object holds coin balance which is accessed by functions in the upgrade manager script

---
+++ ProjecT-Kinesis/Assets/Scripts/Game Management Scripts/Coins.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.VisualScripting;
+using UnityEngine.UI;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
+using System.Reflection;
+
+
+[CreateAssetMenu(fileName = "Coins", menuName = "Persistence")]
+public class Coins : ScriptableObject
+{
+    public new string name;
+    public int coinbalance;
+}
