b37b631cab90b24aa68753d60e0a1b64b6236dab
Multimaste1 <multimaste1@gmail.com>
2025-01-31 10:11:42 +0000

    Updated UpgradeManagement Script
    Script now includes all functions for upgrades; now need a way for values of scriptable objects to carry through scenes.

--- ProjecT-Kinesis/Assets/Scripts/Game Management Scripts/UpgradesManagement.cs
+++ ProjecT-Kinesis/Assets/Scripts/Game Management Scripts/UpgradesManagement.cs
@@ -17,17 +17,23 @@ public class UpgradesManagement : MonoBehaviour
 
     public TextMeshProUGUI healthCosttext;
     public TextMeshProUGUI flightCosttext;
+    public TextMeshProUGUI sizeCosttext;
+    public TextMeshProUGUI velocityCosttext;
 
 
     public Sprite[] upgradeBars; //array to store the different levels of the upgrade bar
     public Image healthupgradeBar; //image component that specifically stores the health upgrade bar; dragged in from the inspector
-    //public Image flightupgradeBar;
+    public Image flightupgradeBar;
+    public Image sizeupgradeBar;
+    public Image velocityupgradeBar;
 
     //stores position of level in the array
 
     public Coins coins; //allows upgrades to access the Coins scriptable object
     public HealthUpgrade health; //creates instance of healthUpgrade scriptable object
-    //public FlightUpgrade flight;
+    public FlightUpgrade flight;
+    public SizeUpgrade size;
+    public VelocityUpgrade velocity;
 
 
 
@@ -45,17 +51,18 @@ public class UpgradesManagement : MonoBehaviour
     {
         coinstext.text = "Coins: " + Mathf.FloorToInt(coins.coinbalance).ToString();
         healthCosttext.text = "Cost: " + Mathf.FloorToInt(health.UpgradeCost).ToString();
-        //flightCosttext.text = "Cost: " + Mathf.FloorToInt(flight.UpgradeCost).ToString();
-
-        //sizeCosttext.text = "Cost: " + Mathf.FloorToInt(sizeCost).ToString();
-        //velocityCosttext.text = "Cost: " + Mathf.FloorToInt(velocityCost).ToString();
+        flightCosttext.text = "Cost: " + Mathf.FloorToInt(flight.UpgradeCost).ToString();
+        sizeCosttext.text = "Cost: " + Mathf.FloorToInt(size.UpgradeCost).ToString();
+        velocityCosttext.text = "Cost: " + Mathf.FloorToInt(velocity.UpgradeCost).ToString();
 
         healthupgradeBar.sprite = upgradeBars[health.UpgradeLevel];
-        //flightupgradeBar.sprite = upgradeBars[flight.UpgradeLevel];
+        flightupgradeBar.sprite = upgradeBars[flight.UpgradeLevel];
+        sizeupgradeBar.sprite = upgradeBars[size.UpgradeLevel];
+        velocityupgradeBar.sprite = upgradeBars[velocity.UpgradeLevel];
     }
     public void upgradeHealth() //function to upgradehealth
     {
-        if (health.UpgradeLevel < upgradeBars.Length && coins.coinbalance >= (health.UpgradeCost)) //if max level (5) isn't reached and coin balance >= cost
+        if (health.UpgradeLevel < upgradeBars.Length-1 && coins.coinbalance >= (health.UpgradeCost)) //if max level (5) isn't reached and coin balance >= cost
         {
             
             healthupgradeBar.sprite = upgradeBars[health.UpgradeLevel];
@@ -80,30 +87,78 @@ public class UpgradesManagement : MonoBehaviour
     }
 
 
-    //public void upgradeFlight() //function to upgradehealth
-    //{
-    //    if (flight.UpgradeLevel < upgradeBars.Length - 1 && coins.coinbalance >= (flight.UpgradeCost)) //if max level (5) isn't reached and coin balance >= cost
-    //    {
+    public void upgradeFlight() //function to upgradehealth
+    {
+        if (flight.UpgradeLevel < upgradeBars.Length - 1 && coins.coinbalance >= (flight.UpgradeCost)) //if max level (5) isn't reached and coin balance >= cost
+        {
 
-    //        flightupgradeBar.sprite = upgradeBars[flight.UpgradeLevel]; //
-    //        flight.UpgradeLevel += 1;
-    //        //increments through upgrade levels
+            flightupgradeBar.sprite = upgradeBars[flight.UpgradeLevel]; //
+            flight.UpgradeLevel += 1;
+            //increments through upgrade levels
 
-    //        coins.coinbalance -= Mathf.FloorToInt(flight.UpgradeCost); //takes away upgrade cost from amount of coins
+            coins.coinbalance -= Mathf.FloorToInt(flight.UpgradeCost); //takes away upgrade cost from amount of coins
+            coinstext.text = "Coins: " + coins.coinbalance.ToString(); //changes coin text to display coin total after upgrade purchase
 
-    //        coinstext.text = "Coins: " + coins.coinbalance.ToString(); //changes coin text to display coin total after upgrade purchase
+
+            flight.UpgradeCost = Mathf.FloorToInt(flight.UpgradeCost * 1.5f);
+            flightCosttext.text = "Cost: " + flight.UpgradeCost.ToString();
+            Debug.Log(coins.coinbalance);
+            UpdateUI();
+        }
+        else //if max level (5) is reached 
+        {
+            Debug.Log("max level");
+            return; //exits the function
+        }
+    }
+
+    public void upgradeSize() //function to upgradehealth
+    {
+        if (size.UpgradeLevel < upgradeBars.Length - 1 && coins.coinbalance >= (size.UpgradeCost)) //if max level (5) isn't reached and coin balance >= cost
+        {
+
+            sizeupgradeBar.sprite = upgradeBars[size.UpgradeLevel]; //
+            size.UpgradeLevel += 1;
+            //increments through upgrade levels
+
+            coins.coinbalance -= Mathf.FloorToInt(size.UpgradeCost); //takes away upgrade cost from amount of coins
+            coinstext.text = "Coins: " + coins.coinbalance.ToString(); //changes coin text to display coin total after upgrade purchase
 
 
-    //        flight.UpgradeCost = Mathf.FloorToInt(flight.UpgradeCost * 1.5f);
-    //        flightCosttext.text = "Cost: " + flight.UpgradeCost.ToString();
-    //        Debug.Log(coins.coinbalance);
-    //        UpdateUI();
-    //    }
-    //    else //if max level (5) is reached 
-    //    {
-    //        Debug.Log("max level");
-    //        return; //exits the function
-    //    }
-    //}
+            size.UpgradeCost = Mathf.FloorToInt(size.UpgradeCost * 1.5f);
+            sizeCosttext.text = "Cost: " + size.UpgradeCost.ToString();
+            Debug.Log(coins.coinbalance);
+            UpdateUI();
+        }
+        else //if max level (5) is reached 
+        {
+            Debug.Log("max level");
+            return; //exits the function
+        }
+    }
 
+    public void upgradeVelocity() //function to upgradehealth
+    {
+        if (velocity.UpgradeLevel < upgradeBars.Length - 1 && coins.coinbalance >= (velocity.UpgradeCost)) //if max level (5) isn't reached and coin balance >= cost
+        {
+
+            velocityupgradeBar.sprite = upgradeBars[velocity.UpgradeLevel]; //
+            velocity.UpgradeLevel += 1;
+            //increments through upgrade levels
+
+            coins.coinbalance -= Mathf.FloorToInt(velocity.UpgradeCost); //takes away upgrade cost from amount of coins
+            coinstext.text = "Coins: " + coins.coinbalance.ToString(); //changes coin text to display coin total after upgrade purchase
+
+
+            velocity.UpgradeCost = Mathf.FloorToInt(velocity.UpgradeCost * 1.5f);
+            velocityCosttext.text = "Cost: " + velocity.UpgradeCost.ToString();
+            Debug.Log(coins.coinbalance);
+            UpdateUI();
+        }
+        else //if max level (5) is reached 
+        {
+            Debug.Log("max level");
+            return; //exits the function
+        }
+    }
 }
