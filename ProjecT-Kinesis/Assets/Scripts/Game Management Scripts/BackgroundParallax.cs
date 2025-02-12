6d4b981075171f293865249170f29449232cc71f
Multimaste1 <multimaste1@gmail.com>
2025-02-12 09:28:32 +0000

    Update BackgroundParallax.cs
    Still figuring out how to get it to work

--- ProjecT-Kinesis/Assets/Scripts/Game Management Scripts/BackgroundParallax.cs
+++ ProjecT-Kinesis/Assets/Scripts/Game Management Scripts/BackgroundParallax.cs
@@ -1,25 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 
-public class BackgroundParallax : MonoBehaviour
-{
-    private float length;
-    private float startpos;
-    public GameObject cam;
-    public float parallaxEffect;
-    // Start is called before the first frame update
-    void Start()
-    {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        float distance = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startpos +distance, transform.position.y, transform.position.z);
-    }
-}
