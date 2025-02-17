76ec6a9ccce688abdc83a6e76b7e0e37df928046
Multimaste1 <multimaste1@gmail.com>
2025-02-17 15:20:32 +0000

    Created Score scriptable object

---
+++ ProjecT-Kinesis/Assets/Scripts/Game Management Scripts/Score.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Score", menuName = "Score")]
+public class Score : ScriptableObject
+{
+    public int score;
+}
