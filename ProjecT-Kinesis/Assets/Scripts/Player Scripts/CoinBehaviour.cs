52bb27c87ed43b7a29b5002b378c5d86db23636e
Multimaste1 <multimaste1@gmail.com>
2025-02-17 15:19:02 +0000

    Update CoinBehaviour.cs

--- ProjecT-Kinesis/Assets/Scripts/Player Scripts/CoinBehaviour.cs
+++ ProjecT-Kinesis/Assets/Scripts/Player Scripts/CoinBehaviour.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CoinBehaviour : MonoBehaviour 
 {
     public Coins coins;
+    public Score score;
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             coins.coinbalance += 5;
+            score.score += 50;
+            Debug.Log("score Up");
             Destroy(gameObject);
         }
         else if (collision.gameObject.CompareTag("Orb"))
         {
             coins.coinbalance += 5;
+            score.score += 50;
+            Debug.Log("score Up");
             Destroy(gameObject);
+        }
+        else
+        {
+            return;
         }
     }
 }
