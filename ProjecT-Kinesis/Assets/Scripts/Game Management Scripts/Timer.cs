059e342fa940a2d3af1817565156a4807e89753b
Multimaste1 <multimaste1@gmail.com>
2025-02-17 15:19:15 +0000

    Made timer a scriptable object

---
+++ ProjecT-Kinesis/Assets/Scripts/Game Management Scripts/Timer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Timer", menuName = "Timer")]
+public class Timer : ScriptableObject 
+{
+    public float timerCount;
+}
