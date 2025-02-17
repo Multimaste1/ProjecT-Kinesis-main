c61d7bcd61c79085d7e9fa1f53a0907285dae058
Multimaste1 <multimaste1@gmail.com>
2025-02-17 15:39:34 +0000

    Attempt of burst

--- ProjecT-Kinesis/Assets/Scripts/Enemy Scripts/BossBehaviour.cs
+++ ProjecT-Kinesis/Assets/Scripts/Enemy Scripts/BossBehaviour.cs
@@ -11,14 +11,19 @@ public class BossBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("shoot", 2f, 0.1f); //need to make it a burst attack
+        //InvokeRepeating("shoot", 2f, 0.1f); //need to make it a burst attack
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        
+        timer += Time.deltaTime; //increases timer by time passed between frames, real world seconds
+        if (timer > 2) //how long enemy needs to wait before shooting, afterwards takes 2 seconds to shoot
+        {
+            timer = 0;
+            InvokeRepeating("shoot", 2f, 0.1f);
+        }
     }
 
     void shoot()
