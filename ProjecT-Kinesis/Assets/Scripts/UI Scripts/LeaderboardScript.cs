5d9bda51b507fa18f0d7109ce55819ba9d062bd9
Multimaste1 <multimaste1@gmail.com>
2025-02-18 14:15:55 +0000

    Started script for leaderboard

---
+++ ProjecT-Kinesis/Assets/Scripts/UI Scripts/LeaderboardScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardScript : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("HighScoreEntryContainer");
+        entryTemplate = entryContainer.Find("HighScoreTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        float templateHeight = 20f;
+        for (int i = 0; i < 10; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
+
+            int rank = i + 1;
+            string rankString;
+            switch (rank) 
+            {
+                default: rankString = rank + "TH"; break;
+                case 1: rankString = "1ST";break;
+                case 2: rankString = "2ND"; break;
+                case 3: rankString = "3RD"; break;
+            }
+
+            entryTransform.Find("positionText").GetComponent<Text>().text = rankString;
+
+            int score = 25;
+            entryTransform.Find("scoreText").GetComponent<Text>().text = score.ToString();
+            string name = "YES";
+            entryTransform.Find("nameText").GetComponent<Text>().text = name;
+
+        }
+           
+    }
+}
