789e2c6042bc1511baa6a34584930562a648895a
Multimaste1 <multimaste1@gmail.com>
2025-02-20 16:38:11 +0000

    Added effects of difficulty to enemy health

--- ProjecT-Kinesis/Assets/Scripts/Enemy Scripts/EnemyHealth.cs
+++ ProjecT-Kinesis/Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -1,33 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-
-    public int enemyMaxHealth;
-    public int health;
+    [SerializeField]
+    private int enemyMaxHealth; //boss doesn't have difficulty SO attached to it so I can just change its health in the inspector
+    [SerializeField]
+    private int health;
+    
 
     public Score score;
+    public Difficulty difficulty;
 
     // Start is called before the first frame update
     void Start()
     {
-        health=enemyMaxHealth;
+        switch (difficulty.difficulty) //cases dependning on difficulty selected which change enemy health
+        {
+            case 1: 
+                enemyMaxHealth = 1;  
+                break;
+            case 2: 
+                enemyMaxHealth = 3;  
+                break;
+            case 3:
+                enemyMaxHealth = 5; 
+                break;
+            default: 
+                enemyMaxHealth = 1; 
+                break;
+        }
+        health = enemyMaxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void takedamage(int damage)
+    public void takedamage(int damage, int enemyScore) //values for damage and enemyScore parameters are passed through in ReflectedBehaviour script
     {
         health = health - damage;
         if (health <= 0)
         {
-            score.score +=25;
+            score.score +=enemyScore;
+            Debug.Log("scoreup");
             Destroy(gameObject);
         }
     }
