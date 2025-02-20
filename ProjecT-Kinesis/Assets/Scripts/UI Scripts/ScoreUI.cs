e3203fc2995b8442fa367b5a9e4b9cccfadbfc8f
Multimaste1 <multimaste1@gmail.com>
2025-02-20 16:40:05 +0000

    Update ScoreUI.cs
    Attempt at increasing score after player lives a certain amount of time; should be awarded coins instead

--- ProjecT-Kinesis/Assets/Scripts/UI Scripts/ScoreUI.cs
+++ ProjecT-Kinesis/Assets/Scripts/UI Scripts/ScoreUI.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class ScoreUI : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public Score score;
+    public Timer timer;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,10 @@ public class ScoreUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (timer.timerCount % 30 == 0) //score increases after every 30 seconds
+        {
+            score.score += 20;
+        }
         scoreText.text = "Score: " + score.score.ToString("0");
     }
 }
