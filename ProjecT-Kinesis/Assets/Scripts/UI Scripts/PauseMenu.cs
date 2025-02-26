1d284970ba12e46ecaa64b37c27c5467a44e2f3e
Multimaste1 <multimaste1@gmail.com>
2025-02-26 13:26:49 +0000

    Update PauseMenu.cs

--- ProjecT-Kinesis/Assets/Scripts/UI Scripts/PauseMenu.cs
+++ ProjecT-Kinesis/Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
-    public GameObject pauseMenuUI;
-    // Start is called before the first frame update
+    public static bool GameIsPaused = false; //bool variable for whether game is paused or not; carries between scripts
+    public GameObject pauseMenuUI; //stores PauseMenu Canvas
+   
    
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)) //when escape key is pressed
         {
-            if (GameIsPaused)
+            if (GameIsPaused) //if GameIsPaused = true
             {
-                Resume();
+                Resume(); //when escape key is pressed game is resumed
             }
             else
             {
-                Pause();
+                Pause(); //when escape key is pressed game is paused
             }
         }
     }
     
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
-        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false); //disables pause menu
+        Time.timeScale = 1f; //time in game is paused
         GameIsPaused = false;
     }
 
     void Pause()
     {
-        pauseMenuUI.SetActive(true);
-        Time.timeScale= 0f;
+        pauseMenuUI.SetActive(true); //enables pause menu
+        Time.timeScale= 0f; //time in game is no longer paused
         GameIsPaused = true;
     }
 }
